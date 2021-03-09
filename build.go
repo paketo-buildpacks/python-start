44bacdfdba5e995d3fc3d257ec44ea19db84d9fe
Arjun Sreedharan <asreedharan@vmware.com>
2021-03-09 16:49:43 +0000

    add go doc

--- build.go
+++ build.go
@@ -5,6 +5,10 @@ import (
 	"github.com/paketo-buildpacks/packit/scribe"
 )
 
+// Build will return a packit.BuildFunc that will be invoked during the build
+// phase of the buildpack lifecycle.
+//
+// Build assigns the image a launch process to run the Python REPL.
 func Build(logger scribe.Logger) packit.BuildFunc {
 	return func(context packit.BuildContext) (packit.BuildResult, error) {
 		logger.Title("%s %s", context.BuildpackInfo.Name, context.BuildpackInfo.Version)
--- detect.go
+++ detect.go
@@ -2,10 +2,20 @@ package pythonstart
 
 import "github.com/paketo-buildpacks/packit"
 
+// BuildPlanMetadata is the buildpack specific data included in build plan
+// requirements.
 type BuildPlanMetadata struct {
+
+	// Launch flag requests the given requirement be made available during the
+	// launch phase of the buildpack lifecycle.
 	Launch bool `toml:"launch"`
 }
 
+// Detect will return a packit.DetectFunc that will be invoked during the
+// detect phase of the buildpack lifecycle.
+//
+// This buildpack will always pass detection.
+// It will require "cpython" as launch-time build plan requirements.
 func Detect() packit.DetectFunc {
 	return func(context packit.DetectContext) (packit.DetectResult, error) {
 		return packit.DetectResult{
